4179f58be7e76bfe71be95fa3b5b31eac9d0b888
Ayush Garg <ayushgargemail@gmail.com>
2022-11-09 16:22:57 -0800

    moviefest2

---
+++ cses/sortingsearching/moviefest2/moviefest2.cpp
@@ -0,0 +1,44 @@
+// https://cses.fi/problemset/task/1632
+
+// TODO
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MAX_N 2e5
+
+vector<pair<int, int>> movies(MAX_N);
+multiset<int> endings;
+
+int main()
+{
+  int n, k;
+  cin >> n >> k;
+
+  for (int i = 0; i < n; ++i)
+    cin >> movies[i].first >> movies[i].second;
+
+  sort(movies.begin(), movies.begin() + n, [](const auto &a, const auto &b)
+       { if (a.second == b.second)
+      return a.first < b.first;
+    return a.second < b.second; });
+
+  for (int i = 0; i < k; ++i)
+    endings.insert(0);
+
+  int ans = 0;
+  for (int i = 0; i < n; i++)
+  {
+    auto it = endings.upper_bound(movies[i].first);
+    if (it == endings.begin())
+      continue;
+
+    endings.erase(--it);
+    endings.insert(movies[i].second);
+    ++ans;
+  }
+
+  cout << ans << '\n';
+
+  return 0;
+}
