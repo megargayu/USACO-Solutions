3e314ec2c655139f11a4214b8da1b810a470e12e
Ayush Garg <ayushgargemail@gmail.com>
2022-11-20 23:17:29 -0800

    added faster moocast solution

---
+++ contests/gold/2016/december/moocast/moocastfast.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MAX_N 1000
+#define pii pair<int, int>
+#define f first
+#define s second
+#define ll long long
+
+vector<pii> cows(MAX_N);
+vector<vector<int>> grid(MAX_N, vector<int>(MAX_N));
+vector<bool> visited(MAX_N);
+int found = 0;
+int N;
+ll X;
+
+ll square(ll num)
+{
+  return num * num;
+}
+
+void dfs(int v)
+{
+  visited[v] = true;
+  ++found;
+
+  for (int i = 0; i < N; i++)
+  {
+    if (visited[i] || grid[v][i] > X)
+      continue;
+    dfs(i);
+  }
+}
+
+bool works()
+{
+  fill(visited.begin(), visited.end(), false);
+  found = 0;
+  dfs(0);
+
+  return found == N;
+}
+
+int main()
+{
+  ifstream fin("moocast.in");
+  ofstream fout("moocast.out");
+
+  fin >> N;
+  for (int i = 0; i < N; ++i)
+    fin >> cows[i].f >> cows[i].s;
+
+  for (int i = 0; i < N; ++i)
+    for (int j = 0; j < N; ++j)
+      grid[i][j] = square(cows[j].f - cows[i].f) + square(cows[j].s - cows[i].s);
+
+  ll lo = 0, hi = 1e10, ans;
+  while (lo <= hi)
+  {
+    X = lo + (hi - lo) / 2LL;
+
+    if (works())
+    {
+      ans = X;
+      hi = X - 1;
+    }
+    else
+      lo = X + 1;
+  }
+
+  fout << ans << '\n';
+}
